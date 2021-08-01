3aa16982edc58d69a7ce9c7e8b1d51666cc50e23
sonangrai <shonahangrae@gmail.com>
2021-08-01 20:08:52 +0545

    card stylings

--- components/card/BlogCard.js
+++ components/card/BlogCard.js
@@ -1,18 +1,23 @@
 import React from "react";
 import DateFormatter from "../../utils/DateFormatter";
 import styles from "./Blogcard.module.scss";
+import Link from "next/link";
 
 const BlogCard = ({ data }) => {
   return (
     <div className={styles.blogCard}>
-      <img src={data.featured_image_url} alt={data.title.rendered} />
-      <div className={styles.overBoard}>
-        <div className={styles.top}>
-          <span className={styles.cat}>{data.post_categories[0].name}</span>
-          <span className={styles.date}>{DateFormatter(data.date)}</span>
-        </div>
-        <div className={styles.title}>{data.title.rendered}</div>
-      </div>
+      <Link href={`/blog/${data.id}`}>
+        <a>
+          <img src={data.featured_image_url} alt={data.title.rendered} />
+          <div className={styles.overBoard}>
+            <div className={styles.top}>
+              <span className={styles.cat}>{data.post_categories[0].name}</span>
+              <span className={styles.date}>{DateFormatter(data.date)}</span>
+            </div>
+            <div className={styles.title}>{data.title.rendered}</div>
+          </div>
+        </a>
+      </Link>
     </div>
   );
 };
